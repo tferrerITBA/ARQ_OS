0a864eda51300c3658ede53eb6a83968a0368801
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2018-05-07 20:01:55 -0300

    Function definitions changed

--- Kernel/include/memoryManager.h
+++ Kernel/include/memoryManager.h
@@ -30,13 +30,13 @@ struct memBlock {   //Bloque de memoria pedido por un proceso
 };
 
 void initializePages();
-void * malloc(size_t size, pid_t pid);
+void * malloc(size_t size);
 m_block getDataBlock(size_t size, m_block mb);
 p_block getProcessBlock(pid_t pid);
 p_block addProcessBlock(pid_t pid);
 void free(void * ptr);
-void * calloc(size_t size, pid_t pid);
-void * realloc(void * ptr, size_t size, pid_t pid);
+void * calloc(size_t size);
+void * realloc(void * ptr, size_t size);
 void joinDataBlocks(m_block m1, m_block m2);
 void * popPage();
 void * initializeProcessStack();
