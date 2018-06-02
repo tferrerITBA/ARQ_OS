da7efd8d4fa4d753564e143b2f0ecf88171b0451
FelipeGorostiaga <fgorostiaga@itba.edu.ar>
2018-06-02 17:07:21 -0300

    Buddy Allocator

---
+++ Kernel/include/buddy.h
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <unistd.h>
+
+#define HEADER_SIZE 8
+#define MIN_ALLOC_LOG2 4
+#define MIN_ALLOC ((size_t)1 << MIN_ALLOC_LOG2)
+#define MAX_ALLOC_LOG2 31
+#define MAX_ALLOC ((size_t)1 << MAX_ALLOC_LOG2)
+#define BUCKET_COUNT (MAX_ALLOC_LOG2 - MIN_ALLOC_LOG2 + 1)
+
+typedef struct list_t {
+  struct list_t *prev, *next;
+} list_t;
+
+
+int update_max_ptr(uint8_t *new_value);
+void list_init(list_t *list);
+void list_push(list_t *list, list_t *entry);
+void list_remove(list_t *entry);
+list_t *list_pop(list_t *list);
+uint8_t *ptr_for_node(size_t index, size_t bucket);
+size_t node_for_ptr(uint8_t *ptr, size_t bucket);
+int parent_is_split(size_t index);
+void flip_parent_is_split(size_t index);
+size_t bucket_for_request(size_t request);
+int lower_bucket_limit(size_t bucket);
+void *malloc(size_t request);
+void free(void *ptr);
+uint8_t * sbrk(size_t size);
+void * realloc(void * ptr, size_t size);
+void * calloc(size_t size);
+
+
+
+
+
+
